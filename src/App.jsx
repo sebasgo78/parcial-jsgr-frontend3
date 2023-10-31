5a641e350d07e931fdb1221b3108e6db2137eb1d
Sebasgo2050 <sebasgo2050@gmail.com>
2023-10-30 20:47:03 -0500

    preparacion parcial front end III

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./Card";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  );
+}
+
+export default App;
